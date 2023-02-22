3fde1a278c9cf5696103afb65e4e454a6f5cdd95
Matthew Moss <matthew@premonition.io>
2023-02-23 10:19:53 +1100

    Update publish script to include major version

--- publish.sh
+++ publish.sh
@@ -4,8 +4,15 @@ set -euo pipefail
 IFS=$'\n\t'
 
 VERSION=$1
+MAJOR_VERSION=$(echo $VERSION | sed -r 's/([0-9]+)\..*/\1/')
 
-docker buildx build .\
-  --platform linux/amd64 \
-  --tag mdmoss/completed-linkerd-job-cleaner:$VERSION \
-  --push
+read -p "Publish version $VERSION (plus major version $MAJOR_VERSION) to Docker Hub? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker buildx build .\
+        --platform linux/amd64 \
+        --tag mdmoss/completed-linkerd-job-cleaner:$VERSION \
+        --tag mdmoss/completed-linkerd-job-cleaner:$MAJOR_VERSION \
+        --push
+fi
